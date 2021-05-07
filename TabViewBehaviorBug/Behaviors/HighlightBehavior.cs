73b322ed5df752a5520b2d040834dcb2c0e2bfb7
Alexey Gulevich <alexey.gulevich@temsa.by>
2021-05-07 17:53:48 +0300

    Add project files.

---
+++ TabViewBehaviorBug/Behaviors/HighlightBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace TabViewBehaviorBug.Behaviors
+{
+    public class HighlightBehavior : Behavior<TextBlock>
+    {
+        private SolidColorBrush _defaultBrush = new SolidColorBrush(Colors.Black);
+        private FontWeight _defaultFontWeight = FontWeights.Normal;
+
+        private SolidColorBrush _highlitedBrush = new SolidColorBrush(Colors.Blue);
+        private FontWeight _highlightedFontWeight = FontWeights.Bold;
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.PointerEntered += OnPointerEntered;
+            AssociatedObject.PointerExited += OnPointerExited;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PointerEntered -= OnPointerEntered;
+            AssociatedObject.PointerExited -= OnPointerExited;
+        }
+
+        private void OnPointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            AssociatedObject.Foreground = _highlitedBrush;
+            AssociatedObject.FontWeight = _highlightedFontWeight;
+        }
+
+        private void OnPointerExited(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            AssociatedObject.Foreground = _defaultBrush;
+            AssociatedObject.FontWeight = _defaultFontWeight;
+        }
+    }
+}
---
+++ TabViewBehaviorBug/SecondPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TabViewBehaviorBug
+{
+    public sealed partial class SecondPage : Page
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack) Frame.GoBack();
+        }
+    }
+}
